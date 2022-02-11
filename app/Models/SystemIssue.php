4af32d184056a8f468a933a3c41c7bdce2c42c04
saldeek <shefaaaldeek@gmail.com>
2022-02-11 01:26:33 +0000

    Modify models

--- app/Models/SystemIssue.php
+++ app/Models/SystemIssue.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class SystemIssue extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'reporter_id',
+        'reporter_description',
+        'reviewer_id',
+        'reviewer_note',
+        'status',
+        'solved',
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
