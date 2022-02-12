7f05d2d012848025b6959b2e2f73c60f046ce882
Rama AlHafez <prog.ramahafez@gmail.com>
2022-02-11 22:53:03 -0800

    update

--- app/Models/Friend.php
+++ app/Models/Friend.php
@@ -14,6 +14,6 @@ class Friend extends Model
     ];
     public function user()
     {
-        return $this->belongsTo(User::class ,'id');
+        return $this->belongsTo(User::class ,'user_id');
     }
 }
