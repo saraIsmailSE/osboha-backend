890711b97ad3d1a54e85a2a08a38ed1543a36079
unknown <bfaidi@gmail.com>
2022-04-11 20:03:56 +0300

    remove stats

--- app/Models/Mark.php
+++ app/Models/Mark.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\Stats\Traits\HasStats;
+
 
 class Mark extends Model
 {
-    use HasFactory,HasStats;
+    use HasFactory;
 
     protected $fillable = [
         'out_of_90', 
