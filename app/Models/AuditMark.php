bc4e543987d0ac1be502572e6ae29d041d1b8e67
HebhAlawam <yaheaawam3@gmail.com>
2022-05-29 08:45:00 +0300

    add relations to audit markgit

--- app/Models/AuditMark.php
+++ app/Models/AuditMark.php
@@ -9,7 +9,19 @@ class AuditMark extends Model
 {
     use HasFactory;
     protected $fillable = [ 
-        'leader_id', 'aduitMarks', 'note', 'status'
+        'leader_id', 'aduitMarks', 'note', 'status' ,'week_id', 'aduitor_id'
     ];
+
+    public function week()
+    {
+        return $this->belongsTo(Week::class);
+    }
+
+    //each audit marks belongsTo specific aduitor(user)
+    public function aduitor()
+    {
+        return $this->belongsTo(User::class ,'aduitor_id');
+    }
+
 
 }
