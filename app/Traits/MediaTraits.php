4c9dffb41fef6ef1cf736582baa665918d8a6d0b
saraIsmailSE <saraismailse@gmail.com>
2022-08-01 16:09:16 +0300

    check thesis comment

--- app/Traits/MediaTraits.php
+++ app/Traits/MediaTraits.php
@@ -2,50 +2,45 @@
 namespace App\Traits;
 use App\Models\Media;
 use App\Models\Reaction;
-
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
+Use \Carbon\Carbon;
 
 Trait MediaTraits{
 
     function createMedia($media, $type_id , $type){
-        $imageName = time().'.'. $media->extension();  
-        $media->move(public_path('assets/images'), $imageName);    
-        // link media with comment
-        $media = new Media();
-        $media->media = $imageName; 
-        $media->type = 'image'; 
-        $media->user_id= Auth::id();
-        if($type == 'comment'){
-            $media->comment_id = $type_id;
+        try {
+            $imageName = rand(100000, 999999).time() . '.' . $media->extension();
+            $media->move(public_path('assets/images'), $imageName);
+            // link media with comment
+            $media = new Media();
+            $media->media = $imageName;
+            $media->type = 'image';
+            $media->user_id = Auth::id();
+            if ($type == 'comment') {
+                $media->comment_id = $type_id;
+            } elseif ($type == 'post') {
+                $media->post_id = $type_id;
+            } elseif ($type == 'infographicSeries') {
+                $media->infographic_series_id = $type_id;
+            } elseif ($type == 'infographic') {
+                $media->infographic_id = $type_id;
+            } elseif ($type == 'book') {
+                $media->book_id = $type_id;
+            } elseif ($type == 'group') {
+                $media->group_id = $type_id;
+            } elseif ($type == 'reaction') {
+                $media->reaction_id = $type_id;
+                $media->type = $type;
+            } else {
+                return 'Type Npt Found';
+            }
+            $media->save();
+            return $media;
+        } catch (\Error $e) {
+            report($e);
+            return false;
         }
-        elseif($type == 'post'){
-            $media->post_id = $type_id;
-        }
-        elseif($type == 'infographicSeries'){
-            $media->infographic_series_id = $type_id;
-        }
-        elseif($type == 'infographic'){
-            $media->infographic_id = $type_id;
-        }
-        elseif($type == 'book'){
-            $media->book_id = $type_id;
-        }
-        elseif($type == 'group'){
-            $media->group_id = $type_id;
-        }
-        elseif($type == 'reaction'){
-            $media->reaction_id = $type_id;
-            $media->type = $type; 
-            
-        }
-        else{
-            return 'Type Npt Found';
-        }
-
-        
-        $media->save();
-        return $media;  
     }
 
     function updateMedia($media, $media_id){
