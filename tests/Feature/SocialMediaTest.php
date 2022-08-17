fe25e267b2bfb22ff419951519f972460e0fbc36
saraIsmailSE <saraismailse@gmail.com>
2022-08-18 00:28:39 +0300

    ..

--- tests/Feature/SocialMediaTest.php
+++ tests/Feature/SocialMediaTest.php
@@ -13,12 +13,42 @@ class SocialMediaTest extends TestCase
      *
      * @return void
      */
-    public function test_addSocialMedia()
+//     public function test_addSocialMedia()
+//    {
+//         $response = $this->withHeaders([
+//             'Authorization' => 'Bearer 1|PKQmrLQvLZUmYZ8cX6WXU44YcpI4RknzYhJQJ483',
+//         ])->post('/socialMedia/add-social-media', ['facebook' => 'Test Social Media']);
+
+//         $response->assertStatus(200);
+//     }
+
+//     /**
+//      * A basic feature test example.
+//      *
+//      * @return void
+//      */
+//     public function test_show()
+//    {
+//         $response = $this->withHeaders([
+//             'Authorization' => 'Bearer 1|PKQmrLQvLZUmYZ8cX6WXU44YcpI4RknzYhJQJ483',
+//         ])->post('/socialMedia/show', ['user_id' => 30]);
+            
+//         $response->assertStatus(200);
+
+//     }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_show()
    {
         $response = $this->withHeaders([
             'Authorization' => 'Bearer 1|PKQmrLQvLZUmYZ8cX6WXU44YcpI4RknzYhJQJ483',
-        ])->post('/socialMedia/add-social-media', ['facebook' => 'Test Social Media']);
+        ])->post('/socialMedia/show', ['user_id' => 4509]);
+            
+        $response->assertStatus(404);
 
-        $response->assertStatus(200);
     }
 }
