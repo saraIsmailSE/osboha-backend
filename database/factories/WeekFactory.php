b1c4ff93689a3bf76637072d7db62a98c78fa535
Asmaa Hamid <asmaa99haim@gmail.com>
2022-08-19 17:50:30 +0300

    edit dates

--- database/factories/WeekFactory.php
+++ database/factories/WeekFactory.php
@@ -16,13 +16,9 @@ class WeekFactory extends Factory
     {
         $title = $this->faker->sentence(2);
         $is_vacation = $this->faker->numberBetween(0, 1);
-        $date_range = Carbon::createFromTimestamp($this->faker->dateTimeBetween($startDate = '-2 months', $endDate = 'now')->getTimeStamp())->startOfWeek(Carbon::SUNDAY);
-        $datetime = Carbon::createFromFormat('Y-m-d H:i:s', $date_range)->addWeeks();
         return [
             'title' => rtrim($title, '.'),
             'is_vacation' => $is_vacation,
-            'created_at' => $datetime,
-            'updated_at' => $datetime
         ];
     }
 }
--- database/seeders/ThesisSeeder.php
+++ database/seeders/ThesisSeeder.php
@@ -11,6 +11,7 @@ use App\Models\ThesisType;
 use App\Models\User;
 use App\Models\Week;
 use App\Traits\ThesisTraits;
+use Carbon\Carbon;
 use Carbon\Factory;
 use Faker\Factory as FakerFactory;
 use Illuminate\Database\Seeder;
@@ -41,61 +42,71 @@ class ThesisSeeder extends Seeder
         //         'is_acceptable' => array_rand($arr) ? date("Y-m-d H:i:s", $timestamp) : null,
         //     ]);
         // }
+        $number = 10;
         $posts = Post::where('type_id', 2)->get();
+        $datetime = Carbon::now()->startOfMonth()->subMonth(2);
+        // $date_range = Carbon::createFromTimestamp($this->faker->dateTimeBetween($startDate = '-2 months', $endDate = 'now')->getTimeStamp());
+        // $datetime = Carbon::createFromFormat('Y-m-d H:i:s', '2022-01-23 11:53:20');        
+        for ($j = 0; $j < $number; $j++) {
+            $date = $datetime->startOfWeek(Carbon::SUNDAY)->addWeeks();
+            Week::factory(1)->create([
+                'created_at' => $date,
+                'updated_at' => $date,
+            ])->each(function ($week) use ($posts) {
+                $users = User::where('is_excluded', 0)->where('is_hold', 0)->get();
+                for ($i = 0; $i < count($users); $i++) {
+                    Mark::factory(1)->create([
+                        'user_id' => $users[$i]->id,
+                        'week_id' => $week->id
+                    ])->each(function ($mark) use ($users, $i, $posts) {
+                        $post_id = $posts[rand(0, count($posts) - 1)]->id;
+                        Comment::factory(rand(1, 5))->create([
+                            'type' => 'thesis',
+                            'user_id' => $users[$i],
+                            'post_id' => $post_id,
+                        ])->each(function ($comment) use ($users, $i, $mark, $post_id) {
+                            $book = Book::where('post_id', $post_id)->first();
 
-        Week::factory(10)->create()->each(function ($week) use ($posts) {
-            $users = User::where('is_excluded', 0)->where('is_hold', 0)->get();
-            for ($i = 0; $i < count($users); $i++) {
-                Mark::factory(1)->create([
-                    'user_id' => $users[$i]->id,
-                    'week_id' => $week->id
-                ])->each(function ($mark) use ($users, $i, $posts) {
-                    $post_id = $posts[rand(0, count($posts) - 1)]->id;
-                    Comment::factory(rand(1, 5))->create([
-                        'type' => 'thesis',
-                        'user_id' => $users[$i],
-                        'post_id' => $post_id,
-                    ])->each(function ($comment) use ($users, $i, $mark, $post_id) {
-                        $book = Book::where('post_id', $post_id)->first();
+                            Thesis::factory(1)->create([
+                                'comment_id' => $comment->id,
+                                'book_id' => $book->id,
+                                'mark_id' => $mark->id,
+                                'user_id' => $users[$i]
+                            ])->each(function ($thesis) use ($mark) {
+                                $thesis_type = ThesisType::find($thesis->type_id)->first()->type;
+                                $total_pages = $thesis->total_pages;
+                                $max_length = $thesis->max_length;
+                                $total_screenshots = $thesis->total_screenshots;
 
-                        Thesis::factory(1)->create([
-                            'comment_id' => $comment->id,
-                            'book_id' => $book->id,
-                            'mark_id' => $mark->id,
-                            'user_id' => $users[$i]
-                        ])->each(function ($thesis) use ($mark) {
-                            $thesis_type = ThesisType::find($thesis->type_id)->first()->type;
-                            $total_pages = $thesis->total_pages;
-                            $max_length = $thesis->max_length;
-                            $total_screenshots = $thesis->total_screenshots;
+                                $new_mark = 0;
+                                if ($thesis_type == 'normal') {
+                                    $new_mark = $this->calculate_mark_for_normal_thesis($total_pages, $max_length, $total_screenshots);
+                                } else {
+                                    $new_mark = $this->calculate_mark_for_ramadan_thesis($total_pages, $max_length, $total_screenshots, $thesis_type);
+                                }
 
-                            $new_mark = 0;
-                            if ($thesis_type == 'normal') {
-                                $new_mark = $this->calculate_mark_for_normal_thesis($total_pages, $max_length, $total_screenshots);
-                            } else {
-                                $new_mark = $this->calculate_mark_for_ramadan_thesis($total_pages, $max_length, $total_screenshots, $thesis_type);
-                            }
+                                $mark->total_pages += $total_pages;
+                                $mark->total_screenshot += $total_screenshots;
+                                $mark->total_thesis += ($max_length > 0 ? 1 : 0);
+                                $mark->out_of_90 += $new_mark;
 
-                            $mark->total_pages += $total_pages;
-                            $mark->total_screenshot += $total_screenshots;
-                            $mark->total_thesis += ($max_length > 0 ? 1 : 0);
-                            $mark->out_of_90 += $new_mark;
+                                if ($mark->out_of_90 > 90) {
+                                    $mark->out_of_90 = 90;
+                                }
+                                $mark->out_of_100 = $mark->out_of_90;
 
-                            if ($mark->out_of_90 > 90) {
-                                $mark->out_of_90 = 90;
-                            }
-                            $mark->out_of_100 = $mark->out_of_90;
+                                if ($mark->support == SUPPORT_MARK && $mark->out_of_100 > 0) {
+                                    $mark->out_of_100 += SUPPORT_MARK;
+                                }
 
-                            if ($mark->support == SUPPORT_MARK && $mark->out_of_100 > 0) {
-                                $mark->out_of_100 += SUPPORT_MARK;
-                            }
-
-                            $mark->save();
+                                $mark->save();
+                            });
                         });
                     });
-                });
-            }
-        });
+                }
+            });
+        }
+
 
         // Thesis::factory(10)->create();
     }
