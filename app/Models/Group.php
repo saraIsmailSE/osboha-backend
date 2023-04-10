a219c9d290ada350d04d2b9088a555f536b2cde1
saraIsmailSE <saraismailse@gmail.com>
2023-04-10 06:55:38 +0300

    update group where pivot null

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -23,28 +23,28 @@ class Group extends Model
 
 
     public function users(){
-        return $this->belongsToMany(User::class,'user_groups')->withPivot('user_type','termination_reason');
+        return $this->belongsToMany(User::class,'user_groups')->whereNull('user_groups.termination_reason')->withPivot('user_type','termination_reason');
     }
     public function userAmbassador(){
-        return $this->belongsToMany(User::class,'user_groups')->withPivot('user_type')->wherePivot('user_type','ambassador');
+        return $this->belongsToMany(User::class,'user_groups')->whereNull('user_groups.termination_reason')->withPivot('user_type')->wherePivot('user_type','ambassador');
     }
     public function groupLeader(){
-        return $this->belongsToMany(User::class,'user_groups')->withPivot('user_type')->wherePivot('user_type','leader')->latest()->take(1);
+        return $this->belongsToMany(User::class,'user_groups')->whereNull('user_groups.termination_reason')->withPivot('user_type')->wherePivot('user_type','leader')->latest()->take(1);
     }
     public function groupSupervisor(){
-        return $this->belongsToMany(User::class,'user_groups')->withPivot('user_type')->wherePivot('user_type','supervisor')->latest()->take(1);
+        return $this->belongsToMany(User::class,'user_groups')->whereNull('user_groups.termination_reason')->withPivot('user_type')->wherePivot('user_type','supervisor')->latest()->take(1);
     }
     public function groupAdvisor(){
-        return $this->belongsToMany(User::class,'user_groups')->withPivot('user_type')->wherePivot('user_type','advisor')->latest()->take(1);
+        return $this->belongsToMany(User::class,'user_groups')->whereNull('user_groups.termination_reason')->withPivot('user_type')->wherePivot('user_type','advisor')->latest()->take(1);
     }
     public function groupAdministrators(){
-        return $this->belongsToMany(User::class,'user_groups')->withPivot('user_type')->wherePivotIn('user_type',['advisor','supervisor','leader']);
+        return $this->belongsToMany(User::class,'user_groups')->whereNull('user_groups.termination_reason')->withPivot('user_type')->wherePivotIn('user_type',['advisor','supervisor','leader']);
     }
     public function leaderAndAmbassadors(){
-        return $this->belongsToMany(User::class,'user_groups')->withPivot('user_type')->wherePivotIn('user_type',['ambassador','leader']);
+        return $this->belongsToMany(User::class,'user_groups')->whereNull('user_groups.termination_reason')->withPivot('user_type')->wherePivotIn('user_type',['ambassador','leader']);
     }
     public function admin(){
-        return $this->belongsToMany(User::class,'user_groups')->withPivot('user_type')->wherePivot('user_type','admin');
+        return $this->belongsToMany(User::class,'user_groups')->whereNull('user_groups.termination_reason')->withPivot('user_type')->wherePivot('user_type','admin');
     }
 
     public function Timeline(){
