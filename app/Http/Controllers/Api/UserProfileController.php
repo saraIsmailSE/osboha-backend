6bb0c4f09b4ed1953a9e0ebd74f672e858c1cdaa
saraIsmailSE <saraismailse@gmail.com>
2023-07-18 02:10:36 +0300

    -

--- app/Http/Controllers/Api/UserProfileController.php
+++ app/Http/Controllers/Api/UserProfileController.php
@@ -288,7 +288,7 @@ class UserProfileController extends Controller
     {
         $response['week'] = Week::latest()->first();
         $group_id = UserGroup::where('user_id', Auth::id())->where('user_type', 'ambassador')->pluck('group_id')->first();
-        $follow_up_group = Group::with('leaderAndAmbassadors')->where('id', $group_id)->first();
+        $follow_up_group = Group::with('users')->where('id', $group_id)->first();
         $response['group_week_avg']= $this->groupAvg($group_id,  $response['week']->id, $follow_up_group->leaderAndAmbassadors->pluck('id'));
 
         $response['week_mark'] = Mark::where('week_id', $response['week']->id)->where('user_id', $user_id)->first();
