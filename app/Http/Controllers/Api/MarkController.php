ecde337f5db239672977e058e295450ef140a3af
saraIsmailSE <saraismailse@gmail.com>
2023-11-04 18:25:40 +0300

    -

--- app/Http/Controllers/Api/MarkController.php
+++ app/Http/Controllers/Api/MarkController.php
@@ -532,7 +532,16 @@ class MarkController extends Controller
             ->orderBy('max_total_pages', 'desc')
             ->limit(53)->get();
         });
-        
+        $response['max_total_thesis'] = Cache::remember('max_total_thesis_in_month', now()->addHours(24), function () use ($startOfMonth, $endOfMonth) {
+            return Mark::with('user')
+            ->where('is_freezed', 0)
+            ->select('user_id', DB::raw('max(total_thesis) as max_total_thesis'))
+            ->whereBetween('created_at', [$startOfMonth, $endOfMonth])
+            ->groupBy('user_id')
+            ->orderBy('max_total_thesis', 'desc')
+            ->limit(53)->get();
+        });
+
         return $this->jsonResponseWithoutMessage($response, 'data', 200);
     }
 
