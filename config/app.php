f629cc8bf768c787cd8d81a2af7f866dfd091613
saraIsmailSE <saraismailse@gmail.com>
2023-11-07 16:43:00 +0300

    -

--- config/app.php
+++ config/app.php
@@ -177,7 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Maatwebsite\Excel\ExcelServiceProvider::class,
+       // Maatwebsite\Excel\ExcelServiceProvider::class,
 
     ],
 
