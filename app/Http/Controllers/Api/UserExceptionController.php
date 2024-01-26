8b7f61525e0a844f89091b5d021cb72b6d24466c
saraIsmailSE <saraismailse@gmail.com>
2024-01-26 19:30:59 +0300

    freeze bug

--- app/Http/Controllers/Api/UserExceptionController.php
+++ app/Http/Controllers/Api/UserExceptionController.php
@@ -127,7 +127,7 @@ class UserExceptionController extends Controller
                     ->whereHas('type', function ($query) {
                         $query->where('type', config('constants.FREEZE_THIS_WEEK_TYPE'))
                             ->orWhere('type', config('constants.FREEZE_NEXT_WEEK_TYPE'));
-                    })->pluck('end_at')->first();
+                    })->pluck('created_at')->first();
 
                 $dateAfter4Weeks = Carbon::parse($laseFreezing)->addWeeks(4)->format('Y-m-d');
 
