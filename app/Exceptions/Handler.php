b8c2db58d132c94a3c82173984b912ab4133d070
saraIsmailSE <saraismailse@gmail.com>
2024-11-27 07:59:13 +0300

    websocket config updates - logs update

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -13,7 +13,8 @@ class Handler extends ExceptionHandler
      * @var array<int, class-string<Throwable>>
      */
     protected $dontReport = [
-        //
+        \App\Exceptions\NotAuthorized::class,
+        \App\Exceptions\NotFound::class,
     ];
 
     /**
